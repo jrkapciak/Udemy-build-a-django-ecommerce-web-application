1d73e4959a5b61855fe857a52ea3416a245fd50d
jrkapciak <jerzyryszardkapciak@gmail.com>
2017-12-03 22:58:47 +0100

    Added custom model manager.

--- src/products/models.py
+++ src/products/models.py
@@ -13,12 +13,21 @@ def upload_image_path(instance,filename):
     final_name ='{}.{}'.format(new_file_name,ext)
     return 'products/{}/{}'.format(new_file_name,final_name)
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=8,default=39.99)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- src/products/views.py
+++ src/products/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 from django.views.generic import ListView,DetailView
 from .models import Product
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = 'products/list.html'
 
     def get_context_data(self,*args,**kwargs):
@@ -11,6 +10,27 @@ class ProductListView(ListView):
         print(context)
         return context
 
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
     template_name = 'products/detail.html'
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        return context
+
+    # def get_object(self,*args,**kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     instance = Product.objects.get_by_id(pk)
+    #     if instance is None:
+    #         raise Http404("Product doesn't exist")
+    #     return instance
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        return Product.objects.filter(pk=pk)
+
