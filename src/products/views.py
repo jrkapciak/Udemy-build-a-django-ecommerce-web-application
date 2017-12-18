7f4e2827118d2bcf50ceed61145e7903357569e3
jrkapciak <jerzyryszardkapciak@gmail.com>
2017-12-18 13:41:24 +0100

    Navbar changes and integration with views

--- src/products/views.py
+++ src/products/views.py
@@ -5,11 +5,6 @@ from .models import Product
 class ProductListView(ListView):
     template_name = 'products/list.html'
 
-    def get_context_data(self,*args,**kwargs):
-        context = super(ProductListView,self).get_context_data(*args,**kwargs)
-        print(context)
-        return context
-
     def get_queryset(self, *args, **kwargs):
         request = self.request
         return Product.objects.all()
--- src/search/views.py
+++ src/search/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from products.models import Product
+
+
+class SearchProductView(ListView):
+    template_name = 'search/view.html'
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args,**kwargs)
+        context['query'] = self.request.GET.get('q')
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        method_dict = request.GET
+        query = method_dict.get('q')
+        if query is not None:
+            return Product.objects.filter(title__icontains=query)
+        return Product.objects.none()
