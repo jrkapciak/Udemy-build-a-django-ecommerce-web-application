7b5bce2548253557256bee5d527d5b3b7dcfa38c
jrkapciak <jerzyryszardkapciak@gmail.com>
2018-01-09 17:38:27 +0100

    Payment module WIP

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'search',
     'tags',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- src/orders/models.py
+++ src/orders/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+from carts.models import Cart
+
+
+ORDER_STATUS_CHOICES = (
+    ('created','Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refunded', 'Refunded'),
+
+)
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+   # billing_profile =
+    #shipping_address
+    #billing address
+    cart = models.ForeignKey(Cart)
+    status = models.CharField(max_length=120,
+                              default='Created',
+                              choices=ORDER_STATUS_CHOICES)
+    shipping_total = models.DecimalField(default=5.99,
+                                         max_digits=10,
+                                         decimal_places=2)
+    total = models.DecimalField(default=0.00,
+                                max_digits=12,
+                                decimal_places=2)
+
+
+    def __str__(self):
+        return self.order_id
