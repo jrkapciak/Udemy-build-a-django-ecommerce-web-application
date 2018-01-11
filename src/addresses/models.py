5d7608f3534a6680185790a82914a40e2d6b127f
jrkapciak <jerzyryszardkapciak@gmail.com>
2018-01-11 19:09:15 +0100

    Address ap

---
+++ src/addresses/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+class Address(models.Model):
--- src/carts/views.py
+++ src/carts/views.py
@@ -8,6 +8,7 @@ from orders.models import Order
 from products.models import Product
 from .models import Cart
 
+
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {"cart": cart_obj})
@@ -25,35 +26,24 @@ def cart_update(request):
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
         else:
-            cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+            cart_obj.products.add(product_obj)
         request.session['cart_items'] = cart_obj.products.count()
-        # return redirect(product_obj.get_absolute_url())
-    return redirect("cart:home")
-
+        return redirect("cart:home")
 
 
 def checkout_home(request):
     cart_obj, cart_created = Cart.objects.new_or_get(request)
     order_obj = None
     if cart_created or cart_obj.products.count() == 0:
-        return redirect("cart:home")  
-    user = request.user
-    billing_profile = None
+        return redirect("cart:home")
+
     login_form = LoginForm()
     guest_form = GuestForm()
-    guest_email_id = request.session.get('guest_email_id')
-    if user.is_authenticated():
-        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
-                                                        user=user, email=user.email)
-    elif guest_email_id is not None:
-        guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
-        billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(
-                                        email=guest_email_obj.email)
-    else:
-        pass
-    
+
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(
+        request)
     if billing_profile is not None:
-        order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
+        order_obj = Order.objects.new_or_get(billing_profile, cart_obj)
 
     context = {
         "object": order_obj,
@@ -62,6 +52,7 @@ def checkout_home(request):
         "guest_form": guest_form
     }
     return render(request, "carts/checkout.html", context)
+
 
 
 
